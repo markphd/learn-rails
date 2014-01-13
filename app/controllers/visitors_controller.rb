5645ed36aeb0378777d8e24961afb4d83b48526d
Mark Philip Dawal <radical.mark@gmail.com>
2014-01-14 01:35:34 +0400

    update application layout

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,6 +1,11 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
+		flash.now[:notice] = "Welcome!"
+		flash.now[:alert] = "My birthday is coming soon!"
 		render 'visitors/new'
 	end
+	# flash.each do |key, value|
+	# 	puts '<div class="' + key + '"/>' + value + '</div>'
+	# end
 end
