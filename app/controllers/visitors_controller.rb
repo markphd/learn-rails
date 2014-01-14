9108863bf3704d758669d98ca1930d445d02b3c8
Mark Philip Dawal <radical.mark@gmail.com>
2014-01-14 04:11:11 +0400

    Front-end framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,11 +1,5 @@
 class VisitorsController < ApplicationController
 	def new
 		@owner = Owner.new
-		flash.now[:notice] = "Welcome!"
-		flash.now[:alert] = "My birthday is coming soon!"
-		render 'visitors/new'
 	end
-	# flash.each do |key, value|
-	# 	puts '<div class="' + key + '"/>' + value + '</div>'
-	# end
 end
